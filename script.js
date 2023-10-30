945cbd56066041da7f51c4f8a217b5cd35a62c48
tastas000 <TiiLg0o000@gmail.com>
2023-10-31 04:21:47 +0700

    Refactor code

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-const Board = document.querySelector("#board");
-const Gameboard = (function () {
+const board = document.querySelector("#board");
+const gameBoard = (function () {
   const players = [player("x"), player("o")];
   const cells = new Array(9);
-  const winnerMove = ["123", "456", "789", "147", "258", "369", "159", "357"];
+  const winnerMove = [[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]];
   let playerTurn = players[Math.floor(Math.random() * 2)];
 
   const getCell = (i) => cells[i];
@@ -13,7 +13,7 @@ const Gameboard = (function () {
   const checkGameOver = () => {
     //check the winner (from stackoverflow)
     for (let move of winnerMove) {
-      if (move.split("").every((e) => playerTurn.getMove().includes(e))) {
+      if (move.every((e) => playerTurn.getMove().includes(e))) {
         alert(`${playerTurn.getName()} wins`);
         return restartGame();
       }
@@ -29,8 +29,8 @@ const Gameboard = (function () {
   };
   const clearBoard = () => {
     [players[0], players[1]] = [player("x"), player("o")];
-    while (Board.firstChild) {
-      Board.removeChild(Board.firstChild);
+    while (board.firstChild) {
+      board.removeChild(board.firstChild);
     }
     cells.fill(undefined);
     playerTurn = players[Math.floor(Math.random() * 2)];
@@ -44,12 +44,12 @@ const Gameboard = (function () {
         if (!getCell(cell.id - 1)) {
           setCell(cell.id - 1, playerTurn.getName());
           cell.textContent = playerTurn.getName();
-          playerTurn.addMove(cell.id);
+          playerTurn.addMove(+cell.id);
           checkGameOver();
           nextPlayer();
         }
       });
-      Board.appendChild(cell);
+      board.appendChild(cell);
     }
   };
   renderBoard();
