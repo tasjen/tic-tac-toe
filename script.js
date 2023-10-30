e5c2a92bb44300a93c545bff41c11d5d7ac3aba6
tastas000 <TiiLg0o000@gmail.com>
2023-10-31 05:13:18 +0700

    Add a replay button

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const gameBoard = (function () {
   const board = document.querySelector("#board");
   const turn = document.querySelector('#turn');
+  const replay = document.querySelector("#replay");
+  
   const players = [player("X"), player("O")];
   const cells = new Array(9);
   const winnerMove = [[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]];
@@ -10,25 +12,29 @@ const gameBoard = (function () {
   const setCell = (i, player) => (cells[i] = player);
   const nextPlayer = () => {
     playerTurn = players[(players.indexOf(playerTurn) + 1) % players.length];
-
+    turn.textContent = `${playerTurn.getName()}'s turn`;
   };
   const checkGameOver = () => {
     //check the winner (from stackoverflow)
     for (let move of winnerMove) {
       if (move.every((e) => playerTurn.getMove().includes(e))) {
         turn.textContent = `${playerTurn.getName()} wins`;
-        return restartGame();
+        replay.style.display = "block";
+        return;
       }
     }
     if (!cells.includes(undefined)) {
       turn.textContent = "Tie"
-      return restartGame();
+      replay.style.display = "block";
+      return;
     }
+    nextPlayer();
   };
   const restartGame = () => {
     clearBoard();
     renderBoard();
   };
+  replay.addEventListener("click", restartGame);
   const clearBoard = () => {
     [players[0], players[1]] = [player("X"), player("O")];
     while (board.firstChild) {
@@ -36,6 +42,7 @@ const gameBoard = (function () {
     }
     cells.fill(undefined);
     playerTurn = players[Math.floor(Math.random() * 2)];
+    replay.style.display = "none";
   };
   const renderBoard = () => {
     for (let i = 0; i < 9; i++) {
@@ -43,13 +50,11 @@ const gameBoard = (function () {
       cell.id = i + 1;
       cell.addEventListener("click", () => {
         //if the cell is not taken by any player
-        if (!getCell(cell.id - 1)) {
+        if (!getCell(cell.id - 1) && !(replay.style.display === 'block')) {
           setCell(cell.id - 1, playerTurn.getName());
           cell.textContent = playerTurn.getName();
           playerTurn.addMove(+cell.id);
           checkGameOver();
-          nextPlayer();
-          turn.textContent = `${playerTurn.getName()}'s turn`;
         }
       });
       board.appendChild(cell);
